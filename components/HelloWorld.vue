8bbe549f6fe170073183109c9092b694dfa2f249
Piyush <thesourcepedia@gmail.com>
2022-11-04 16:39:55 +0530

    test: success run and build with auto import

---
+++ components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>Hello World</div>
+    <div>Calc {{ total }}</div>
+  </div>
+</template>
+<script lang="ts">
+export default defineComponent({
+  setup() {
+    const num1 = ref(1);
+    const num2 = ref(1);
+    const total = num1.value + num2.value;
+
+    return { total };
+  },
+});
+</script>
